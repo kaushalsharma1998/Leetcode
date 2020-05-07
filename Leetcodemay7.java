61e36aad9ac36dab59b872bab60e9050f3973bae
kaushalsharma1998 <kaushalsharma1198@gmail.com>
2020-05-07 14:50:05 +0530

    2nd commit

--- Leetcodemay7.java
+++ Leetcodemay7.java
@@ -3,7 +3,9 @@ public class Leetcodemay7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("intial commit");
+System.out.println("2nd commit"
+		+ " commit"
+		+"pass");
 	}
 
 }
