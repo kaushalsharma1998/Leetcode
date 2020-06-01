c04acf31ddaa01ff98b30eeeb44d3d45955b21de
kaushalsharma1998 <kaushalsharma1198@gmail.com>
2020-06-02 02:57:31 +0530

    june challenge 01

---
+++ LeetcodeJUNEchallenge/Leetcodejune01.java
@@ -0,0 +1,63 @@
+package LeetcodeJUNEchallenge;
+import LeetcodeMAYchallenge.Leetcodemay07.TreeNode;
+public class Leetcodejune01 {
+
+	
+	
+	
+
+	
+
+		
+		/**
+		 * Definition for a binary tree node.
+		 * public class TreeNode {
+		 *     int val;
+		 *     TreeNode left;
+		 *     TreeNode right;
+		 *     TreeNode() {}
+		 *     TreeNode(int val) { this.val = val; }
+		 *     TreeNode(int val, TreeNode left, TreeNode right) {
+		 *         this.val = val;
+		 *         this.left = left;
+		 *         this.right = right;
+		 *     }
+		 * }
+		 */
+
+		    public TreeNode invertTree(TreeNode root) {
+		    if (root == null) {
+		        return null;
+		    }
+		    TreeNode right = invertTree(root.right);
+		    TreeNode left = invertTree(root.left);
+		    root.left = right;
+		    root.right = left;
+		    return root;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		}
+
+	
+
+	
+	
+	
+	
+	
+}
