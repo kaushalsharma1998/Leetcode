22ec7a650697b0469087f9e228f7e949e00d5794
Kaushal Sharma <kaushalsharma1998@gmail.com>
2020-08-03 01:41:41 +0530

    update

---
+++ LeetcodeAUGchallenge/aug2.java
@@ -0,0 +1,21 @@
+class MyHashSet {
+
+    boolean[] a;
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        a = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        a[key] = true;
+    }
+    
+    public void remove(int key) {
+        a[key] = false;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return a[key];
+    }
+}
